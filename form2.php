91a08eeb30915f1f38e68e0e37d8521da5bf6278
barul <barul+git@barul.info>
2018-03-14 14:43:10 +0100

    updated the query part

--- form2.php
+++ form2.php
@@ -1,10 +1,15 @@
 <?php
-$db = new SQLite3('db/db.sqlite');
+$db = new PDO("sqlite:db/db.sqlite");
 if (isset($_POST["name"])){
 	$ename = $_POST["name"];
 	$eweight = $_POST["weight"];
 	$edate = date('Ymd');
-	$db->exec("INSERT INTO exercise (name, weight, date) VALUES('$ename', '$eweight', '$edate');");
+	$statement = $db->prepare("INSERT INTO exercise (name, weight, date) VALUES(:ename, :eweight, :edate)");
+	$statement->bindParam(':ename', $_POST["name"], SQLITE3_TEXT);
+	$statement->bindParam(':eweight', $_POST["weight"], SQLITE3_TEXT);
+	$statement->bindParam(':edate', $edate);
+	$statement->execute();
+	
 }
 // Go to the index after writing into the db
 header('Location: index.php');
